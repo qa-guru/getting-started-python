8ff991dd1842388a3c01fd7772e6fd3a84734e84
alx.krw <sashkir7@mail.ru>
2023-02-17 21:31:56 +0600

    Fix search by phras

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -3,5 +3,5 @@ from selene import be, have
 
 
 browser.open('https://google.com')
-browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
+browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
 browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
