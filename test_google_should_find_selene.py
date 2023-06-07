89e6a7a2a0c887cb381da32e5d641806256ce696
Ekaterina.Torsunova <katrin.tep@list.ru>
2023-06-07 12:39:47 +0300

    Fixture and test2 added

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -1,7 +1,20 @@
+from time import sleep
+
 from selene.support.shared import browser
 from selene import be, have
+import pytest
+
+@pytest.fixture()
+def brows():
+    browser.open('https://google.com')
+
+def test1(brows):
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+    print ("По запросу найдена ссылка")
 
 
-browser.open('https://google.com')
-browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+def test2(brows):
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    assert browser.element('[id="search"]') != 'qmkugnj ,.'
+    print ("По запросу ничего не найдено")
