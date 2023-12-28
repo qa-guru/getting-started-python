37f3c2efc9b89c4a40ab408f2bd5bd78203f9218
yashaka <yashaka@gmail.com>
2023-12-28 18:55:35 +0200

    add pytest version of test

---
+++ test_google.py
@@ -0,0 +1,7 @@
+from selene import browser, be, have
+
+
+def test_selene_can_be_found():
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
