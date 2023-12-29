6607f4d58dfee0de3746ab34b8a5c025bc057466
yashaka <yashaka@gmail.com>
2023-12-29 13:58:13 +0200

    tune test to google confirmation after open

--- test_google.py
+++ test_google.py
@@ -1,7 +1,11 @@
-from selene import browser, be, have
+from selene import browser, be, have, by
 
 
 def test_selene_can_be_found():
     browser.open('https://google.com')
+    if browser.element(by.text('Accept all')).matching(be.visible):
+        browser.element(by.text('Accept all')).click()
+
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
