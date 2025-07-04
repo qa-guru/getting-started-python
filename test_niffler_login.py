b85ae52062ad6db71cfa209db85687066815500f
svasenkov <aanher@gmail.com>
2025-07-04 16:04:50 +0200

    added code from lesson

--- test_niffler_login.py
+++ test_niffler_login.py
@@ -1,46 +1,26 @@
 from selene import browser, have
 
-def test_succesfull_login():
+def test_success_login():
     browser.open('https://niffler.qa.guru')
-    browser.element('input[name="username"]').type('stas')
-    browser.element('input[name="password"]').type('12345')
-    browser.element('button[type="submit"]').click()
+    browser.element('[id="username"]').type('stas')
+    browser.element('[id="password"]').type('12345')
+    browser.element('[id="login-button"]').click()
 
     browser.element('[id="spendings"]').should(have.text('History of Spendings'))
     browser.quit()
 
-def test_succesfull_login_with_enter():
+def test_success_login_with_press_enter():
     browser.open('https://niffler.qa.guru')
-    browser.element('input[name="username"]').type('stas')
-    browser.element('input[name="password"]').type('12345').press_enter()
+    browser.element('[id="username"]').type('stas')
+    browser.element('[id="password"]').type('12345').press_enter()
 
     browser.element('[id="spendings"]').should(have.text('History of Spendings'))
     browser.quit()
 
-def test_unsuccesfull_login_with_wrong_credentials():
+def test_wrong_credentials():
     browser.open('https://niffler.qa.guru')
-    browser.element('input[name="username"]').type('stas')
-    browser.element('input[name="password"]').type('123456').press_enter()
+    browser.element('[id="username"]').type('stas')
+    browser.element('[id="password"]').type('12345sgsrgwr').press_enter()
 
     browser.element('[class="form__error"]').should(have.text('Bad credentials'))
-    browser.quit()
-
-def test_succesfull_login_check_username():
-    browser.open('https://niffler.qa.guru')
-    browser.element('input[name="username"]').type('stas')
-    browser.element('input[name="password"]').type('12345').press_enter()
-    browser.element('[id="spendings"]').should(have.text('History of Spendings'))
-
-    # todo check username in profile
-    browser.element('todo').should(have.text('todo'))
-    browser.quit()
-
-def test_succesfull_logout():
-    browser.open('https://niffler.qa.guru')
-    browser.element('input[name="username"]').type('stas')
-    browser.element('input[name="password"]').type('12345').press_enter()
-    browser.element('[id="spendings"]').should(have.text('History of Spendings'))
-
-    # todo check logout
-    browser.element('todo').should(have.text('todo'))
     browser.quit()
