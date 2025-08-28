5b1c6d8f046863f8511f477ffca3fb7b8792bb59
Vasenkov Stanislav <aanher@gmail.com>
2025-08-29 00:06:21 +0700

    Update google_should_find_selene.py
    updated search phrase

--- google_should_find_selene.py
+++ google_should_find_selene.py
@@ -2,7 +2,7 @@ from selene import browser, be, have
 
 
 browser.open('https://google.com')
-browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+browser.element('[name="q"]').should(be.blank).type('qa.guru').press_enter()
 browser.element('html').should(have.text('About this page'))
 
-# browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+# browser.element('[id="search"]').should(have.text('QA.GURU: Курсы тестировщиков'))
