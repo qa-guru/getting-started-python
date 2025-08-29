a5c3cd7f7c8bb79d48acc64a343f1c17f009436c
svasenkov <aanher@gmail.com>
2025-08-29 11:32:20 +0200

    code from lesson

--- google_should_find_selene.py
+++ google_should_find_selene.py
@@ -1,8 +1,9 @@
 from selene import browser, be, have
 
 
-browser.open('https://google.com')
-browser.element('[name="q"]').should(be.blank).type('qa.guru').press_enter()
-browser.element('html').should(have.text('About this page'))
+def test_search():
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('qa.guru').press_enter()
+    browser.element('html').should(have.text('About this page'))
 
 # browser.element('[id="search"]').should(have.text('QA.GURU: Курсы тестировщиков'))
--- test_niffler_login.py
+++ test_niffler_login.py
@@ -1,26 +1,8 @@
-from selene import browser, have
+from selene import browser, be, have
+
 
 def test_success_login():
-    browser.open('https://niffler.qa.guru')
-    browser.element('[id="username"]').type('stas')
-    browser.element('[id="password"]').type('12345')
-    browser.element('[id="login-button"]').click()
-
-    browser.element('[id="spendings"]').should(have.text('History of Spendings'))
-    browser.quit()
-
-def test_success_login_with_press_enter():
-    browser.open('https://niffler.qa.guru')
-    browser.element('[id="username"]').type('stas')
+    browser.open("https://niffler.qa.guru")
+    browser.element('[id="username"]').should(be.blank).type('stas')
     browser.element('[id="password"]').type('12345').press_enter()
-
     browser.element('[id="spendings"]').should(have.text('History of Spendings'))
-    browser.quit()
-
-def test_wrong_credentials():
-    browser.open('https://niffler.qa.guru')
-    browser.element('[id="username"]').type('stas')
-    browser.element('[id="password"]').type('12345sgsrgwr').press_enter()
-
-    browser.element('[class="form__error"]').should(have.text('Bad credentials'))
-    browser.quit()
